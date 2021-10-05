fd88723cf2e6ae15c9f64d439420683293486499
Tony J <tonyweb13@gmail.com>
2021-10-05 22:17:03 +0800

    fix jquery

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -22,7 +22,7 @@
 
     <script type="text/javascript" src="/js/jquery/jquery-3.4.1.min.js"></script>
 
-    <script src="/js/jquery-ui/jquery-ui.js"></script>
+    <script type="text/javascript" src="/js/jquery-ui/jquery-ui.min.js"></script>
 
     <script type="text/javascript" src="/js/popper/popper.min.js"></script>
 
